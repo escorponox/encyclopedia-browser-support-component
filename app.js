9f541c44d6f61df0245a9fa5085dda1452fbf45e
Carlos Coves Prieto <escorponox@gmail.com>
2016-06-20 13:47:56 +0200

    Code Box functionality (#10)
    * browser support animations
    
    * user select none and disable listener during animation
    
    * code box tab click
    
    * removed anonymous functions to improve readability
    
    * toggleBox bubbling event bug fixed

--- app.js
+++ app.js
@@ -1,26 +1,119 @@
 document.addEventListener('DOMContentLoaded', function cb() {
-    [].forEach.call(document.querySelectorAll('.tb--open'), function (el) {
-        el.querySelector('.tb__content').style.display = 'block';
+
+    [].forEach.call(document.querySelectorAll('.cb__buttons__button'), function (el) {
+        el.addEventListener('click', codeBoxButtonClick);
     });
 
     [].forEach.call(document.querySelectorAll('.tb__header'), function (el) {
-        el.addEventListener('click', function (e) {
-            var content = el.nextElementSibling;
-            var container = el.parentNode;
-            animate(content, container);
-        }, false);
+        el.addEventListener('click', animateToggleBox);
     });
 
-    function animate(content, container) {
+    [].forEach.call(document.querySelectorAll('.version.partial-supported .bs__version-radio'), function (el) {
+        el.addEventListener('click', manageVersionClick);
+    });
+
+    [].forEach.call(document.querySelectorAll('.version-info__close'), function (el) {
+        el.addEventListener('click', hideVersionAndContainer.bind(null, el.parentNode.id.slice(0, -5)));
+    });
+
+    function codeBoxButtonClick(event) {
+        if (!event.target.classList.contains('cb__buttons__button--active')) {
+            const buttons = event.target.parentNode.children;
+            const codeBoxChildren = event.target.parentNode.parentNode.children;
+            [].forEach.call(buttons, function (button, index) {
+                if (button === event.target) {
+                    button.classList.add('cb__buttons__button--active');
+                    codeBoxChildren[index + 1].classList.add('cb__code-container--active');
+                }
+                else {
+                    button.classList.remove('cb__buttons__button--active');
+                    codeBoxChildren[index + 1].classList.remove('cb__code-container--active');
+                }
+            });
+        }
+    }
+
+    function manageVersionClick(event) {
+        event.target.removeEventListener('click', manageVersionClick);
+        if (event.target.parentNode.classList.contains('version--selected'))
+            hideVersionAndContainer(event.target.id);
+        else {
+            const anotherSelectedVersion = document.querySelectorAll('.' + event.target.name + ' + .browser--version .version.version--selected .bs__version-radio');
+            changeVersion(event, anotherSelectedVersion);
+            if (!anotherSelectedVersion.length)
+                showVersionAndContainer(event.target.id);
+        }
+    }
+
+    function changeVersion(event, anotherSelectedVersion) {
+        [].forEach.call(anotherSelectedVersion, function (anotherVersionInput) {
+            const oldVersionInfo = document.querySelector('#' + anotherVersionInput.id + '-info');
+            Velocity(oldVersionInfo, "fadeOut", {
+                duration: 200,
+                begin: function () {
+                    anotherVersionInput.parentNode.classList.remove('version--selected');
+                },
+                complete: function () {
+                    oldVersionInfo.classList.remove('version-info--selected');
+                    oldVersionInfo.style = '';
+                    document.querySelector('#' + event.target.id + '-info').classList.add('version-info--selected');
+                    event.target.parentNode.classList.add('version--selected');
+                    event.target.addEventListener('click', manageVersionClick);
+                }
+            });
+
+        });
+    }
+
+    function showVersionAndContainer(versionId) {
+        const versionInfo = document.querySelector('#' + versionId + '-info');
+        Velocity(versionInfo.parentNode, "slideDown", {
+            duration: 400,
+            begin: function () {
+                versionInfo.parentNode.classList.add('info-container--show');
+                versionInfo.classList.add('version-info--selected');
+            },
+            complete: function () {
+                const versionRadio = document.querySelector('#' + versionId);
+                versionRadio.parentNode.classList.add('version--selected');
+                versionRadio.addEventListener('click', manageVersionClick);
+            }
+        });
+    }
+
+    function hideVersionAndContainer(versionId) {
+        const versionRadio = document.querySelector('#' + versionId);
+        const versionInfo = document.querySelector('#' + versionId + '-info');
+        Velocity(versionInfo.parentNode, "slideUp", {
+            duration: 400,
+            begin: function () {
+                versionRadio.checked = false;
+                versionRadio.parentNode.classList.remove('version--selected');
+            },
+            complete: function () {
+                versionInfo.parentNode.classList.remove('info-container--show');
+                versionInfo.classList.remove('version-info--selected');
+                versionRadio.addEventListener('click', manageVersionClick);
+            }
+        });
+    }
+
+    function animateToggleBox(event) {
+        const content = event.currentTarget.nextElementSibling;
+        const container = event.currentTarget.parentNode;
         if (container.classList.contains('tb--open')) {
-            container.classList.remove('tb--open');
             Velocity(content, "slideUp", {
                 duration: 400,
+                complete: function () {
+                    container.classList.remove('tb--open');
+                }
             });
         } else {
-            container.classList.add('tb--open');
             Velocity(content, "slideDown", {
                 duration: 400,
+                complete: function () {
+                    container.classList.add('tb--open');
+                }
             });
         }
     }
